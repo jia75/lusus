63c13581fa2e9c3eb285d1e40084e7b04e5645c5
Jia <61246620+rexillion@users.noreply.github.com>
2023-11-02 00:35:54 +0200

    added basic FEN support: places pieces, sets color to play. Board gets rendered from the start, with startpos as default.

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const standardBoardToBuffered = [21,22,23,24,25,26,27,28,
                                  61,62,63,64,65,66,67,68,
                                  71,72,73,74,75,76,77,78,
                                  81,82,83,84,85,86,87,88,
-                                 91,92,93,94,95,96,97,98,];
+                                 91,92,93,94,95,96,97,98];
+const invertedBoardToStandard = [56,57,58,59,60,61,62,63,
+                                 48,49,50,51,52,53,54,55,
+                                 40,41,42,43,44,45,46,47,
+                                 32,33,34,35,36,37,38,39,
+                                 24,25,26,27,28,29,30,31,
+                                 16,17,18,19,20,21,22,23,
+                                  8, 9,10,11,12,13,14,15,
+                                  0, 1, 2, 3, 4, 5, 6, 7];
 const pieceNames = ["Empty","WhiteKing","WhiteQueen","WhiteRook","WhiteBishop","WhiteKnight","WhitePawn",
 "BlackKing","BlackQueen","BlackRook","BlackBishop","BlackKnight","BlackPawn","Border"];
+const pieceSymbolToCode = ["K","Q","R","B","N","P","k","q","r","b","n","p"];
+const colorToCode = ["w","b"];
+const possibleEmptySquareNumbers = ["/","1","2","3","4","5","6","7","8"];
 function boardIndexToFile(index) {
     return index%10 - 1;
 }
@@ -45,6 +56,26 @@ function initializeBoard() {
     boardArray.push(0);
     return boardArray;
 }
+function interpretFEN(FENString) {
+    let FENStringParts = FENString.split(" ");
+    let boardModificationIndex = 0;
+    let FENStringCharacters = FENStringParts[0].split("");
+    boardToReturn = initializeBoard();
+    for (let FENStringAnalysisIndex = 0; FENStringAnalysisIndex < FENStringCharacters.length; FENStringAnalysisIndex++) {
+        if (pieceSymbolToCode.includes(FENStringCharacters[FENStringAnalysisIndex])) {
+            boardToReturn[standardBoardToBuffered[invertedBoardToStandard[boardModificationIndex]]] = pieceSymbolToCode.indexOf(FENStringCharacters[FENStringAnalysisIndex]) + 1;
+            boardModificationIndex++;
+            continue;
+        }
+        if (possibleEmptySquareNumbers.includes(FENStringCharacters[FENStringAnalysisIndex])) {
+            boardModificationIndex += possibleEmptySquareNumbers.indexOf(FENStringCharacters[FENStringAnalysisIndex]);
+            continue;
+        }
+        //if invalid character: return empty board
+        return initializeBoard();
+    }
+    boardToReturn[120] = colorToCode.indexOf(FENStringParts[1]);
+    return boardToReturn;
+}
 
-
-mainBoard = initializeBoard();
+var mainBoard = interpretFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
